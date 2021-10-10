ddd1fc6d6286a2a2e8eaaba9268a7f4eca634e1e
jayatigumber <91008655+jayatigumber@users.noreply.github.com>
2021-10-10 16:20:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 st.header('Movie Recommender System')
 movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+similarity = pandas.read_pickle(open('model/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
